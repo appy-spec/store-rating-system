a86b746bf105b3aa9de843958e39cd3a72fcf87f
Rahul Patel <140371157+appy-spec@users.noreply.github.com>
2026-07-12 23:44:04 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('surpriseButton').addEventListener('click', function() {
-    const surpriseMessage = document.getElementById('surpriseMessage');
-    surpriseMessage.classList.toggle('d-none');
-});
